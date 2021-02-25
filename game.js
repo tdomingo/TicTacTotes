de408b2e2341bc7f4203da43ae6491a6ee5c1fc4
Thomas Domingo <tdomingo90@gmail.com>
2021-02-24 17:42:35 -0800

    refactor - switch row and column labels. Rows now numbered, columns abc

--- game.js
+++ game.js
@@ -7,11 +7,7 @@ const rl = readline.createInterface({
 const mem = {
   player: 1,
   names: {},
-  board: {
-    a: [0, 0, 0],
-    b: [0, 0, 0],
-    c: [0, 0, 0],
-  },
+  board: (new Array(9).fill(0)),
   moves: 0,
 };
 const changePlayer = () => {
@@ -31,43 +27,49 @@ const ask1Name = () => {
   })
 }
 
+const displayLetter = (num) => {
+  if (num === 0) {
+    return ' ';
+  }
+  return num === 1 ? ('\x1b[35m' + 'X' + '\x1b[0m') : '\x1b[36m' + 'O' + '\x1b[0m';
+}
+
 const displayBoard = () => {
   const formatRow = (row) => (
     JSON.stringify(row).slice(1, 6).split(',').map((num) => displayLetter(Number(num))).join(' | ').concat(' :')
   )
-  const displayLetter = (num) => {
-    if (num === 0) {
-      return ' ';
-    }
-    return num === 1 ? ('\x1b[35m' + 'X' + '\x1b[0m') : '\x1b[36m' + 'O' + '\x1b[0m';
-  }
-  const { a, b, c } = mem.board;
+  const { board } = mem
+  const row1 = board.slice(0, 3);
+  const row2 = board.slice(3, 6);
+  const row3 = board.slice(6, 9);
   console.log();
-  console.log('        1   2   3');
+  console.log('        a   b   c');
   console.log('      .............');
-  console.log(`    a : ${formatRow(a)}`);
+  console.log(`    1 : ${formatRow(row1)}`);
   console.log('      : --------- :')
-  console.log(`    b : ${formatRow(b)}`);
+  console.log(`    2 : ${formatRow(row2)}`);
   console.log('      : --------- :')
-  console.log(`    c : ${formatRow(c)}`);
+  console.log(`    3 : ${formatRow(row3)}`);
   console.log('      .............');
   console.log();
 }
 
 const ask2Name = () => {
-  rl.question(`And what should I call you, ${colorizeTextPlayer('player 2', 2)}? \n `, (answer) => {
+  rl.question(`And what should I call you, ${colorizeTextPlayer('player 2', 2)}? \n`, (answer) => {
     mem.names[2] = answer;
     console.log(`... if that is truly your preference, I shall refer to you as '${answer}'.`);
-    console.log('now, let us get down to the task at hand.', '\n');
-    console.log('Here is your board:', '\n')
-    promptMove();
+    setTimeout(()=> {
+      console.log('now, let us get down to the task at hand.', '\n');
+      console.log('Here is your board:', '\n');
+      setTimeout(promptMove, 1000);
+    }, 1000)
   })
 }
 
 const explainGameplay = () => {  console.log("\x1b[32m");
   console.log(`To play, specify the row and column where you'd like to place your piece.`);
   console.log(`responding "a1" will mark the space in the top left corner of the board.`);
-  console.log(`"c2" would mark the middle space in the bottom row.`);
+  console.log(`"b3" would mark the middle space in the bottom row.`);
   console.log("\x1b[0m");
 }
 
@@ -78,23 +80,38 @@ const promptMove = () => {
     explainGameplay();
   }
   const name = mem.names[player];
-  rl.question(`What is your move, ${colorizeTextPlayer(name, player)}? \n`, (answer) => {
-    handleInput(answer)
-  })
+  setTimeout(() => {
+    rl.question(`Where would you like to place your ${displayLetter(player)}, ${colorizeTextPlayer(name, player)}? \n`, (answer) => {
+      handleInput(answer)
+    })
+  }, 300)
+}
+
+const colToNum = (s) => {
+  if (s === 'a') {
+    return 0;
+  }
+  if (s === 'b') {
+    return 1;
+  }
+  if (s === 'c') {
+    return 2;
+  }
 }
 
 const handleInput = (input) => {
   const rc = input.split('');
-  const row = rc[0];
-  const col = (Number(rc[1]) - 1);
-  const validRow = (['a', 'b', 'c'].indexOf(row) !== -1);
-  const validCol = (col % 1 === 0 && col >= 0 && col <= 2);
+  const col = rc[0];
+  const row = (Number(rc[1]) - 1);
+  const validCol = (['a', 'b', 'c'].indexOf(col) !== -1);
+  const validRow = (row % 1 === 0 && row >= 0 && row <= 2);
   let { board, player } = mem;
   if (validRow && validCol) {
-    if (board[row][col] !== 0) {
+    const checkIndex = colToNum(col) + (row * 3);
+    if (board[checkIndex] !== 0) {
       repeatInput();
     } else {
-      board[row][col] = player;
+      board[checkIndex] = player;
       mem.moves ++;
       if (mem.moves === 9) {
         displayBoard();
@@ -134,7 +151,7 @@ const badInput = (l) => {
     console.log("\x1b[33m")
     console.log(        'c1')
     console.log("\x1b[0m");
-    setTimeout(promptMove, 2000);
+    setTimeout(promptMove, 1000);
   }, 300);
 }
 
